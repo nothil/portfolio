d1cfb632cba7a08ab4957480dc4081a38eb0e33f
nothile <nothile.ngcobo@umuzi.org>
2019-10-24 16:35:37 +0200

    finishe all linke pages

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+
+        function myFunction() {
+          var x = document.getElementById("CV.pdf");
+          x.disabled = true;
+        }
+        
